c0799d48fc98259ce3fef2c3ce57395200280aed
dengke-li <linuslukia@gmail.com>
2025-12-07 22:18:45 +0100

    Refactor input feed config

--- ingestion/ingest_etl/config.py
+++ ingestion/ingest_etl/config.py
@@ -1,14 +1,14 @@
-public_feeds=("DERNIÈRES ACTUALITÉS de Public.fr : https://www.public.fr/dernieres-actualites/feed,"
-           "PEOPLE : https://www.public.fr/people/feed,"
-           "TÉLÉ : https://www.public.fr/tele/feed,"
-           "FAITS-DIVERS : https://www.public.fr/faits-divers/feed,"
+public_feeds=("DERNIÈRES ACTUALITÉS de Public.fr : https://www.public.fr/dernieres-actualites/feed, "
+           "PEOPLE : https://www.public.fr/people/feed, "
+           "TÉLÉ : https://www.public.fr/tele/feed, "
+           "FAITS-DIVERS : https://www.public.fr/faits-divers/feed, "
            "LIFESTYLE : https://www.public.fr/lifestyle/feed")
 
 vsd_feeds=(
-    "ACTU PEOPLE : https://vsd.fr/actu-people/feed/,"
-   "TÉLÉ : https://vsd.fr/tele/feed/,"
-   "SOCIÉTÉ : https://vsd.fr/societe/feed/,"
-   "CULTURE : https://vsd.fr/culture/feed/,"
+    "ACTU PEOPLE : https://vsd.fr/actu-people/feed/, "
+   "TÉLÉ : https://vsd.fr/tele/feed/, "
+   "SOCIÉTÉ : https://vsd.fr/societe/feed/, "
+   "CULTURE : https://vsd.fr/culture/feed/, "
    "LOISIRS : https://vsd.fr/loisirs/feed/"
 )
 
--- ingestion/ingest_etl/main.py
+++ ingestion/ingest_etl/main.py
@@ -1,42 +1,37 @@
 import os
 import time
 
-#import psycopg
-
 from ingest_etl.crawler import Crawler
 from ingest_etl.sink import DBSink, InMemorySink
 from ingest_etl.config import public_feeds, vsd_feeds
 
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-PUBLIC_FEEDS = os.getenv("public_FEEDS", public_feeds).split(",")
-VSD_FEEDS = os.getenv("VSD_FEEDS", vsd_feeds).split(",")
+PUBLIC_FEEDS = os.getenv("PUBLIC_FEEDS", public_feeds).split(", ")
+VSD_FEEDS = os.getenv("VSD_FEEDS", vsd_feeds).split(", ")
 
-POLL_INTERVAL = int(os.getenv("POLL_INTERVAL_SEC", "300"))
-
-def run_once(crawlers):
-    for c in crawlers:
-        try:
-            c.poll_once()
-        except Exception as e:
-            print("poll error for", c.url, e)
-
+POLL_INTERVAL = int(os.getenv("POLL_INTERVAL_SEC", "30000"))
 
 def main_loop():
-    db_sink = InMemorySink()
+    db_sink = DBSink() #InMemorySink() #
     site_feeds = {'vsd': VSD_FEEDS, 'public': PUBLIC_FEEDS}
     crawlers = []
     for site in site_feeds:
         feeds = site_feeds[site]
         for category_url in feeds:
-            print(category_url.split(' : '))
+            print(category_url)
             category, url = tuple(category_url.split(' : '))
             path = f"data/{site}_{category}.xml"
             crawlers.append(
                 Crawler(url=url, path=path, sink=db_sink),
             )
     while True:
-        run_once(crawlers)
+        print('run crawlers')
+        for c in crawlers:
+            try:
+                c.poll_once()
+            except Exception as e:
+                print("poll error for", c.url, e)
         time.sleep(POLL_INTERVAL)
 
 if __name__ == '__main__':
